5d728bb19bb6658f1d5a165abfd0dcf530834e5f
Mike <free_at_last_08@yahoo.com>
2023-11-01 12:20:40 -0700

    Initial Commit

---
+++ sfr3test/sfr3testApp.swift
@@ -0,0 +1,17 @@
+//
+//  sfr3testApp.swift
+//  sfr3test
+//
+//  Created by Mike on 11/1/23.
+//
+
+import SwiftUI
+
+@main
+struct sfr3testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sfr3testUITests/sfr3testUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  sfr3testUITestsLaunchTests.swift
+//  sfr3testUITests
+//
+//  Created by Mike on 11/1/23.
+//
+
+import XCTest
+
+final class sfr3testUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
